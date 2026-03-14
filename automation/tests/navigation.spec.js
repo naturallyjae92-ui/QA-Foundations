c96657dfb243c8a8e938436710800d2244251b72
Jae <naturallyjae92@gmail.com>
2026-03-13 22:37:37 -0400

    Fix navigation test locator for Example Domain link

--- automation/tests/navigation.spec.js
+++ automation/tests/navigation.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@playwright/test");
 
-test("Navigation: click More information link", async ({ page }) => {
+test("Navigation: open IANA link from Example Domain", async ({ page }) => {
   await page.goto("https://example.com/");
-  await page.getByRole("link", { name: "More information..." }).click();
+  await page.locator("a[href*='iana.org']").click();
   await expect(page).toHaveURL(/iana\.org/);
 });
