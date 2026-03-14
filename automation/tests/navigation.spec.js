7489cf393522046c015fc98313de7ff8d76826ab
Jae <naturallyjae92@gmail.com>
2026-03-13 22:40:39 -0400

    Module 3 Day 1: Add navigation test and stabilize locator

--- automation/tests/navigation.spec.js
+++ automation/tests/navigation.spec.js
@@ -1,5 +1,6 @@
 const { test, expect } = require("@playwright/test");
 
+// Use href-based locator to avoid brittleness from punctuation/ellipsis changes in link text
 test("Navigation: open IANA link from Example Domain", async ({ page }) => {
   await page.goto("https://example.com/");
   await page.locator("a[href*='iana.org']").click();
